3668f26470d5a05485cd66bead93ca27c2db2c0f
Charles Lau <v-chlau1@microsoft.com>
2020-10-11 11:08:09 -0700

    Add new web project

---
+++ Chlau5206.github.io/Default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Chlau5206.github.io {
+    public partial class _Default : Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
---
+++ Chlau5206.github.io/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chlau5206.github.io.Global" Language="C#" %>
