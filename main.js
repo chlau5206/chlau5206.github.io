783cf6545a727e89807fb544d493b39fab9a58d9
ChLau5206 <charlesyounglau@gmail.com>
2022-02-06 16:54:17 -0800

    Update index.html and WhatIsLive.html

---
+++ main.js
@@ -0,0 +1,10 @@
+function subtraction(a,b){
+	var c;
+	c = a-b;
+	return c;
+}
+var z = subtraction(10, 7);
+// document.write("after subtraction, the different is " + z);
+alert("Welcome to Chlau 5206 - GitHub");
+
+
